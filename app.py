f6480b5e09eae76289a38b38084c9b42b68c9ffb
DhrRas <rastogi.dhruv@gmail.com>
2025-04-22 14:02:14 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import mysql.connector
 
 
@@ -16,22 +16,15 @@ def api_details():
         cursor = mydb.cursor()
         cursor.execute('Select Source, title, PublishedDate, unique_id from test1')
         data = cursor.fetchall()
-        return render_template('display.html', data = data)
+        return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', debug = True, port = 5000)
 
 
-## we have to use database data to display the records 
-## as using one after another times api calling is expensive as 
-## it can exhaust your limit of using.
-## use mysql code to display data
-## then make a dropdown for different data and a fetch button
-## that fetches data from database 
-## any format mein show kr skte ho 
-## can tabular, or any.
-
 ## one thing to remember to add filter options like
 ## 1. by 1 day
 ## 2. by that date
-## or something like that. so at last pls maintain option for that.
+## or something like that. 
+# so at last pls maintain option for that.
+# render_template('display.html', data = data)
