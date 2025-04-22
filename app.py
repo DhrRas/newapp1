6e7fa7faf57d8669be8a29b0bee5f5a57ffbf60d
DhrRas <rastogi.dhruv@gmail.com>
2025-04-22 16:11:06 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 
 
@@ -13,14 +13,27 @@ mydb = mysql.connector.connect(
 
 @app.route('/details', methods=['GET'])
 def api_details():
-        cursor = mydb.cursor()
-        cursor.execute('Select Source, title, PublishedDate, unique_id from test1')
-        data = cursor.fetchall()
-        return render_template('display.html', data = data)
+        
+        filter_type = request.args.get('filter')
+        filter_value = request.args.get('value')
 
-def view_method():
-      dropdown_list = ['Air', 'Land', 'Sea']
-      return render_template('display.html', dropdown_list = dropdown_list)
+        cursor = mydb.cursor()
+        query = 'Select Source, title, PublishedDate, unique_id from test1'
+        values = []
+
+        if filter_type  and filter_value:
+              if filter_type == 'data':
+                    query += 'Where PublishedDate = %s'
+                    values.apped(filter.value)
+              elif filter_type == 'source':
+                    query += 'Where Source = %s'
+                    values.append(filter_value)
+        
+        cursor.execute(query, tuple(values))
+        data = cursor.fetchall()
+        return render_template('display.html', data = data, selected_filter = filter_type, filter_value = filter_value)
+
+
 
 
 if __name__ == "__main__":
